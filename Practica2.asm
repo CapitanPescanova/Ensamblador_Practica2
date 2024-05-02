7e7dc7a4599d0bd7b9753917cbcd6994f39a63b9
DESKTOP-ER2MVN3\Victor_Lap <victoresprio@gmail.com>
2024-05-02 19:24:29 +0200

    Subrutina producto ya funciona con la subrutina suma

--- Practica2.asm
+++ Practica2.asm
@@ -2,10 +2,13 @@
 ## DECLARACION ALIAS ##
 #######################
 .eqv _b 2
-.eqv _e 8
+.eqv _e 3
 
 .eqv _aux1, $t0
-.eqv _sum2, $t1
+.eqv _aux2, $t1
+.eqv _aux3, $t2
+.eqv _aux4, $t3
+
 
 ###########################
 ## DECLARACION VARIABLES ##
@@ -18,7 +21,7 @@
 ########################
 .text 
 	add _aux1, $zero, _b
-	add _sum2, $zero, _e
+	add _aux3, $zero, _e
 
 	jal producto_recur
 
@@ -49,10 +52,10 @@
 	sw $ra, 0($sp)
 	
 	#Comprobamos que el segundo sumando sea igual a 0 
-	beqz _sum2, else_sum
+	beqz _aux2, else_sum
 	
 		#Si no es igual a 0 restamos uno al segundo sumando y volvemos a llamar a la subrutina
-		add _sum2, _sum2, -1
+		add _aux2, _aux2, -1
 		
 		#Llamamos a la misma sub-rutina
 		jal suma_recur
@@ -85,17 +88,25 @@
     	addi $sp, $sp, -4
     	sw $ra, 0($sp)
     
-    	# Comprobamos que el segundo producto sea igual a 0 
-   	beqz _sum2, else_product
+    	#Comprobamos que el segundo factor sea igual a 0 
+   	beqz _aux3, else_product
 
-        	# Si no es igual a 0 restamos uno al segundo producto y volvemos a llamar a la subrutina
-        	add _sum2, _sum2, -1
-        
-        	# Llamamos a la misma sub-rutina
-        	jal producto_recur
-        
-        	# Sumamos el valor del primer producto al resultado de la subrutina
-        	add $v1, $v1, _aux1
+        	#Si no es igual a 0 restamos uno al segundo factor y volvemos a llamar a la subrutina
+        	add _aux3, _aux3, -1
+               	jal producto_recur
+        	
+        	#Igualamos el valor del producto resultante al _aux2 para que la subrutina suma_recur pueda trabajar
+        	move _aux2, $v1
+
+        	#Iniciamos _aux3 a 0 y llamamos a la subrutina suma_recur
+        	move _aux3, $zero
+        	jal suma_recur
+
+		#Aprovechamos el registro _aux3 para sumar y acumular los resultados de la subrutina suma_recur
+		add _aux3, _aux3, $v1
+		
+		#igualamos $v1 a _aux3 ya que es la salida de nuestra subrutina
+		move $v1, _aux3
 
         	# Recuperamos $ra de la pila para salir de la subrutina
         	lw $ra, 0($sp)
@@ -107,7 +118,7 @@
 	else_product:
     		# Si el segundo producto es igual a 0 establecemos en $v1 el valor cero
     		li $v1, 0
-    
+   
     		# Recuperamos $ra de la pila para salir de la subrutina
     		lw $ra, 0($sp)
     		addi $sp, $sp, 4
